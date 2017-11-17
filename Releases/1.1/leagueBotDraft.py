6e4638dff6a9ad14809661b6d5df6cf0af0ec721
Thomas Dion <thomasnoidion@gmail.com>
2017-11-17 12:24:29 -0500

    1.1
    Bot allowing discord users to check any league of legends player ranks (from the NA server)

---
+++ Releases/1.1/leagueBotDraft.py
@@ -0,0 +1,33 @@
+import requests, asyncio, discord
+from discord.ext import commands
+# Personal mods
+from lol_crawler import *
+from dbManager import *
+
+# ^^^^^^^^^^^^^#
+
+
+queueMatcherBot = commands.Bot(command_prefix="!", description="queueMatcher v1.1")
+
+
+@queueMatcherBot.event
+async def on_ready():
+    print("CONNECTED")
+
+
+@queueMatcherBot.command(pass_context=True)
+async def setrank(ctx):
+    authorRaw = str(ctx.message.author)
+    author = authorRaw.split('#')[0]
+    authorId = authorRaw.split('#')[1]
+
+
+@queueMatcherBot.command()
+async def rank(*args):
+    s = " "
+    string_args = s.join(args)
+    summonerName = string_args
+    await queueMatcherBot.say(getStats(summonerName))
+
+
+queueMatcherBot.run('Mzc4NjA2MzY0NjU1NjE2MDAw.DO85Xw.8GtG2x0L8I02G5Q9FuiHeFOa6_Q')
---
+++ Releases/1.1/lol_crawler.py
@@ -0,0 +1,31 @@
+import requests
+def requestID(summonerName):
+    url = 'https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/' + summonerName + '?api_key=RGAPI-eaa2b842-0bac-4b0c-8522-5f8744792d0a'
+    getObj = requests.get(url)
+    print(getObj.json())
+    return getObj.json()["id"]
+
+def requestRank(summonerId):
+
+    url = 'https://na1.api.riotgames.com/lol/league/v3/leagues/by-summoner/' + summonerId  + '?api_key=RGAPI-eaa2b842-0bac-4b0c-8522-5f8744792d0a'
+    getObj = requests.get(url)
+    print(getObj.json())
+    return getObj.json()
+
+def getStats(summonerName):
+    print(str(requestID(summonerName)))
+    try:
+        summonerID = str(requestID(summonerName))
+    except:
+
+        return "That IGN is not valid"
+
+    summonerInfos = requestRank(summonerID)
+    try:
+        tier = summonerInfos[0]['tier']
+    except:
+        return "This summoner is unrank, try getting at least b4 this season 8^]"
+
+    rank = summonerInfos[0]['entries'][0]['rank']
+    answer = summonerName + "\ntier: " + tier + "\nrank: " + rank
+    return answer
